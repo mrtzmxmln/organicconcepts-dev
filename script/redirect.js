16fe7a286dc77db3c203e7b5254dceec54c4c2be
Moritz <moritzmaximilian@gmx.net>
2025-07-13 13:31:46 +0200

    added consulting

--- script/redirect.js
+++ script/redirect.js
@@ -6,6 +6,10 @@ function openPool() {
   window.location.href = "../pool/";
 }
 
+function openConsulting() {
+  window.location.href = "../consulting/";
+}
+
 function openInstagram() {
   window.open("https://instagram.com/organicconcepts", "_blank").focus();
 }
