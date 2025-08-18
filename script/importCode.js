6002cf8452c9f4b401359fe75e28bde2a25d8c1a
Moritz <moritzmaximilian@gmx.net>
2025-08-18 21:10:25 +0200

    rebuild backup

---
+++ script/importCode.js
@@ -0,0 +1,14 @@
+loadHTML("../templates/footer.html", "#footer");
+loadHTML("../templates/sidebar.html", "#sidebar");
+loadHTML("../templates/navigation.html", "#navigation");
+
+function loadHTML(url, selector) {
+  fetch(url)
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (html) {
+      document.querySelector(selector).innerHTML = html;
+      updateCartBadge();
+    });
+}
